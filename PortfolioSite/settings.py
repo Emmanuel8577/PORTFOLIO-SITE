918c7f26e4716957d38b365a4bc713c661cf4455
Emmanuel8577 <Emmanueledache54@gmail.com>
2024-04-13 23:20:05 +0100

    collected static files

--- PortfolioSite/settings.py
+++ PortfolioSite/settings.py
@@ -118,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [BASE_DIR / "static"] # new
+STATIC_ROOT = BASE_DIR / "staticfiles" # new
 
 
 MEDIA_URL = '/media/'
